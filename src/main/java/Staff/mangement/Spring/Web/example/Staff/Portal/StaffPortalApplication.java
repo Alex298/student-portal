f8b3c13104b557e374626c1b901173c53a458204
Alex Basson <alexbasson12@gmail.com>
2024-05-05 15:14:34 +0200

    Main code very *mooi*

---
+++ src/main/java/Staff/mangement/Spring/Web/example/Staff/Portal/StaffPortalApplication.java
@@ -0,0 +1,13 @@
+package Staff.mangement.Spring.Web.example.Staff.Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaffPortalApplication.class, args);
+	}
+
+}
